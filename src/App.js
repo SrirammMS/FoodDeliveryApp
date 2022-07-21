916d0731f549d390abd0035489ca75835c94c5d9
Sriramm MS <40908467+SrirammMS@users.noreply.github.com>
2022-07-21 11:10:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import Header from './components/Layout/Header.js';
+import Meals from './components/Meals/Meals.js';
+import Cart from './components/Cart/Cart'
+
+function App() {
+  return (
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
